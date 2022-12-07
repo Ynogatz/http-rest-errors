56f9d1b2e39ff078852482e38d7ac4f5d3e21d87
ynogatz <ynogatzds@gmail.com>
2022-12-07 11:09:04 -0300

    atualizacao versoes libs

--- src/error/generic.error.ts
+++ src/error/generic.error.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express-serve-static-core'
 
 export class GenericError extends Error {
 	public readonly status: number
